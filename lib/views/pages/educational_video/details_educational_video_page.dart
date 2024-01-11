d7ba4258d9ab45e69a93bafdbf620eee6bebbafa
altamisatmaja <altamisfattah16@gmail.com>
2024-01-11 17:33:47 +0700

    feat: details educational

--- lib/views/pages/educational_video/details_educational_video_page.dart
+++ lib/views/pages/educational_video/details_educational_video_page.dart
@@ -1,1 +1,24 @@
 part of '../pages.dart';
+
+class DetailsEducationalVideoPage extends StatelessWidget {
+  const DetailsEducationalVideoPage({
+    Key? key,
+    required this.videoUrl,
+    required this.title,
+  }) : super(key: key);
+
+  final String videoUrl;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    // Implement your detail page UI here
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text('Video URL: $videoUrl'),
+      ),
+    );
+  }
+}
