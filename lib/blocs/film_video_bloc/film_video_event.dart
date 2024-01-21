b1504a199bc14bc34b8c7270cc3d4653ecc0036e
altamisatmaja <altamisfattah16@gmail.com>
2024-01-21 23:52:44 +0700

    init: setup bloc content

---
+++ lib/blocs/film_video_bloc/film_video_event.dart
@@ -0,0 +1,8 @@
+// part of 'infographics_bloc.dart';
+
+part of '../blocs.dart';
+
+// @immutable
+// class InfographicsEvent with _$InfographicsEvent {
+//   const factory InfographicsEvent.started() = _Started;
+// }
