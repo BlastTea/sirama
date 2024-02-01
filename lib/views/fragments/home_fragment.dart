1214c0c95398397d763cfb3965d2f3b71dd1596e
altamisatmaja <altamisfattah16@gmail.com>
2024-02-01 07:36:47 +0700

    fix: home fragment

--- lib/views/fragments/home_fragment.dart
+++ lib/views/fragments/home_fragment.dart
@@ -53,7 +53,7 @@ class _HomeFragmentState extends State<HomeFragment> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           const Text(
-                            'Welcome Home',
+                            'Selamat datang',
                             style: TextStyle(
                                 color: Colors.grey,
                                 fontWeight: FontWeight.bold),
@@ -62,7 +62,7 @@ class _HomeFragmentState extends State<HomeFragment> {
                             height: 10,
                           ),
                           Text(
-                            '${currentUser?.username ?? 'Anonymous'} 👋',
+                            '${currentUser?.username ?? 'Guest'} 👋',
                             style: const TextStyle(
                               fontSize: 28,
                               fontWeight: FontWeight.bold,
@@ -75,8 +75,7 @@ class _HomeFragmentState extends State<HomeFragment> {
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: ClipOval(
-                        child: Image.asset('assets/ugm.png', width: 50),
-
+                        child: Image.asset('assets/person.png', width: 50),
                         //45:00 min
                       ),
                     ),
@@ -87,68 +86,27 @@ class _HomeFragmentState extends State<HomeFragment> {
                 height: 20,
               ),
               const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: MyCarouselSlider()
-                
-                // BlocBuilder<QuoteBloc, QuoteState>(
-                //   builder: (context, stateQuote) {
-                //     if (stateQuote is QuoteInitial) {
-                //       MyApp.quoteBloc.add(InitializeQuoteData());
-                //     }
-
-                //     if (stateQuote is QuoteDataLoaded) {
-                //       final List<String> imgList = stateQuote.quotes
-                //           .map((quote) =>
-                //               "https://dev-sirama.propertiideal.id/storage/quote/${quote.gambarQuote!}")
-                //           .toList();
-
-                //       if (kDebugMode) {
-                //         print(imgList);
-                //       }
-                //       final List<Widget> imageSliders = imgList
-                //           .map((item) => Container(
-                //                   margin: const EdgeInsets.all(5.0),
-                //                   child: ClipRRect(
-                //                     borderRadius:
-                //                         const BorderRadius.all(Radius.circular(5.0)),
-                //                     child: Stack(
-                //                       children: <Widget>[
-                //                         Image.network(item,
-                //                             ),
-                //                       ],
-                //                     ),
-                //                   ),
-                //               ))
-                //           .toList();
-                //       return CarouselSlider(
-                //         options: CarouselOptions(
-                //           height: 200.0,
-                //           autoPlay: true,
-                //           autoPlayInterval: const Duration(seconds: 6),
-                //         ),
-                //         items: imageSliders,
-                //       );
-                //     } else if (stateQuote is QuoteInitial) {
-                //       return const CircularProgressIndicator();
-                //     } else if (stateQuote is QuoteError) {
-                //       return const Text('error anj');
-                //     } else {
-                //       return Container();
-                //     }
-                //   },
-                // ),
-              ),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: MyCarouselSlider()),
               const SizedBox(
                 height: 20,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: CardTile(
-                  title: const Text('Yuk, Tanya Ahli!'),
-                  subtitle: const Text(shortLorem),
-                  image: Image.asset('assets/card-homepage.png', height: 50.0),
+                  title: const Text('Yuk, Skrining!'),
+                  subtitle: const Text(
+                      'Ayo deteksi dini perilaku pencegahan bullying atau perundungan Sobat RAMA'),
+                  button: const Row(
+                    children: [
+                      Text('Skrining'),
+                      SizedBox(width: 10),
+                      Icon(Icons.arrow_forward, color: Colors.white,)
+                    ],
+                  ),
+                  image: Image.asset('assets/card-homepage.png',),
                   imageAlignment: CardTileAlignment.bottom,
-                  minImageWidth: 134.0,
+                  minImageWidth: 100.0,
                   onPressed: () => NavigationHelper.to(MaterialPageRoute(
                       builder: (context) => const ScreeningPage())),
                 ),
@@ -190,15 +148,17 @@ class _HomeFragmentState extends State<HomeFragment> {
                               right: e == 'Tanya Ahli' ? 20.0 : 0.0),
                           child: ActionChip(
                             label: Text(e),
+                            side: BorderSide(
+                                color: Theme.of(context).colorScheme.onPrimary,),
                             color: MaterialStatePropertyAll(
                                 Theme.of(context).colorScheme.primary),
                             labelStyle: Theme.of(context)
                                 .textTheme
                                 .labelLarge
                                 ?.copyWith(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onPrimary),
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                ),
                             onPressed: () => debugPrint('on $e pressed'),
                           ),
                         ),
