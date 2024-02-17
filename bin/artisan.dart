a4a8f405ef7ad3b5e59bc7485362d1130a95bd77
BlastTea <arya.jbr.999@gmail.com>
2024-02-18 01:23:05 +0700

    feat: add toUppercase in artisan in another isolate

--- bin/artisan.dart
+++ bin/artisan.dart
@@ -1,4 +1,38 @@
+import 'dart:io';
+import 'dart:isolate';
+
 void main() async {
-  String result = await Future.delayed(const Duration(days: 365, hours: 6)).then((value) => 'hahahahahahaha');
-  print(result);
+  String message = 'hahahahahahaha';
+
+  ReceivePort receivePortMessage = ReceivePort()..listen((value) => message = value);
+
+  ReceivePort receivePortOnExit = ReceivePort();
+
+  Isolate.spawn(
+    compute,
+    {
+      'send_port_message': receivePortMessage.sendPort,
+      'send_port_on_exit': receivePortOnExit.sendPort,
+      'message': message,
+    },
+  );
+
+  dynamic onExitMessage = await receivePortOnExit.first;
+
+  if (onExitMessage is! String || onExitMessage != 'SUCCESS') throw onExitMessage;
+
+  print(message);
+
+  exit(0);
+}
+
+void compute(Map<String, dynamic> args) {
+  SendPort sendPortOnExit = args['send_port_on_exit'];
+
+  try {
+    (args['send_port_message'] as SendPort).send((args['message'] as String).toUpperCase());
+    sendPortOnExit.send('SUCCESS');
+  } catch (e) {
+    sendPortOnExit.send(e);
+  }
 }
