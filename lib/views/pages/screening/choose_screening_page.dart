ecfc42e3696d30856f1e09439ff2e4fcdefc8524
altamisatmaja <altamisfattah16@gmail.com>
2024-02-20 21:51:38 +0700

    fix: screening

--- lib/views/pages/screening/choose_screening_page.dart
+++ lib/views/pages/screening/choose_screening_page.dart
@@ -3,6 +3,62 @@ part of '../pages.dart';
 class ChooseScreeningPage extends StatelessWidget {
   const ChooseScreeningPage({super.key});
 
+  static Widget listSkrinning({
+    required BuildContext context,
+    required List<Skrinning> skrinnings,
+    int? idSkrinning,
+  }) =>
+      skrinnings.isEmpty
+          ? Center(
+              child: Text(
+                'Tidak ada data',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            )
+          : ListView.builder(
+              shrinkWrap: true,
+              primary: false,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              itemBuilder: (context, index) {return ListTile(
+                title: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blue.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 7),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          skrinnings[index].jenisSkrinning!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold, 
+                            fontSize: 10,
+                          ),
+                        ),
+                        const Icon(Icons.chevron_right),
+                      ],
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/mainscreening');
+                },
+              );
+              });
+
   @override
   Widget build(BuildContext context) => Scaffold(
           body: ListView(
